76eb9896c02bd1728829ab4405a757a9cc17d8dd
necarlson97 <nilscarlson1997@gmail.com>
2025-03-13 15:55:46 -0400

    Rotate after xstart

--- builds/Omnissiah Shrine.sh
+++ builds/Omnissiah Shrine.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+xrandr --output HDMI-1 --rotate inverted
+
 echo -ne '\033c\033]0;Omnissiah Shrine\a'
 base_path="$(dirname "$(realpath "$0")")"
 "$base_path/Omnissiah Shrine.arm64" "$@"
--- go.sh
+++ go.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Script to launch the build in a GIU on the rpi terminal mode
-startx /home/pi/Omnissiah-Shrine/builds/Omnissiah\ Shrine.sh -- :0
+startx "/home/pi/Omnissiah-Shrine/builds/Omnissiah\ Shrine.sh" -- :0
