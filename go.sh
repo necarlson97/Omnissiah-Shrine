c4b86f48c37e0fd9f4a024c6ad4bfb0c39c9c659
Nils Carlson <nilscarlson1997@gmail.com>
2025-03-28 14:55:59 -0400

    Test flip in go.sh

--- go.sh
+++ go.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 # Script to launch the build in a GIU on the rpi terminal mode
-startx ./launch_gui.sh -- :0
+xrandr --output HDMI-1 --rotate inverted
+amixer set Master 100% unmute
+
+startx /home/pi/Omnissiah-Shrine/builds/Omnissiah\ Shrine.sh -- :0
