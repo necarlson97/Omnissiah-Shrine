d71a997cea4983d26cebb52300830f1c72a21cb3
Nils Carlson <nilscarlson1997@gmail.com>
2025-03-28 15:02:49 -0400

    Hide mouse

--- global_inputs.gd
+++ global_inputs.gd
@@ -1,5 +1,8 @@
 extends Node
 
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
 func _input(event: InputEvent):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
